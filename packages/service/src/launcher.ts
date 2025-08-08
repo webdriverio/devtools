e309cab57cb8670cb109a0f740b2d8c5dbfd5e8e
Vishnu Vardhan <vishnu.p@browserstack.com>
2025-08-08 13:29:06 +0530

    Close the browser only when the user manages to close it manually and not when the test runner completes its execution

--- packages/service/src/launcher.ts
+++ packages/service/src/launcher.ts
@@ -1,9 +1,10 @@
 import { remote } from 'webdriverio'
 import { start } from '@wdio/devtools-backend'
-
+import logger from '@wdio/logger'
 import { DEFAULT_LAUNCH_CAPS } from './constants.ts'
 import type { ServiceOptions, ExtendedCapabilities } from './types.js'
 
+const log = logger('@wdio/devtools-service:Launcher')
 export class DevToolsAppLauncher {
   #options: ServiceOptions
   #browser?: WebdriverIO.Browser
@@ -42,7 +43,23 @@ export class DevToolsAppLauncher {
   }
 
   async onComplete () {
-    await this.#browser?.deleteSession()
+    if (this.#browser) {
+      log.info('Please close the browser window to finish...');
+      while (true) {
+        try {
+          await this.#browser.getTitle();
+          await new Promise(res => setTimeout(res, 1000));
+        } catch (e) {
+          log.info('Browser window closed, stopping DevTools app');
+          break;
+        }
+      }
+      try {
+        await this.#browser.deleteSession();
+      } catch (err: any) {
+        log.warn('Session already closed or could not be deleted:', err.message);
+      }
+    }
   }
 
   #updateCapabilities (caps: ExtendedCapabilities[], devtoolsApp: { port: number }) {
