9c8c64866627ff4978128ee24be39e721b811a14
Vishnu Vardhan <vishnu.p@browserstack.com>
2025-11-18 19:46:47 +0530

    Eslint fix

--- packages/app/src/utils/TestRunner.ts
+++ packages/app/src/utils/TestRunner.ts
@@ -1,13 +1,13 @@
 // const RUN_EVENT = 'app-run-tests'
 const VIEW_SOURCE_EVENT = 'app-source-highlight'
 
-export const viewSource = (
-  callSource: string | undefined
-) => {
-  if (!callSource) return
+export const viewSource = (callSource: string | undefined) => {
+  if (!callSource) {
+    return
+  }
   window.dispatchEvent(
     new CustomEvent(VIEW_SOURCE_EVENT, {
-      detail:  callSource
+      detail: callSource
     })
   )
 }
