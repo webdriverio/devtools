c5163fa9cbefbd5be31f1f58b3c8cbeae8403c3c
Vince Graics <wince17@gmail.com>
2026-05-20 19:01:25 +0200

    feat: Add @wdio/elements package
    - Migrates browser element detection, accessibility tree, and mobile element locator generation from the MCP project into a standalone, reusable package

---
+++ packages/elements/src/index.ts
@@ -0,0 +1,17 @@
+export { getInteractableBrowserElements } from './browser-elements.js'
+export type {
+  BrowserElementInfo,
+  GetBrowserElementsOptions
+} from './browser-elements.js'
+
+export { getBrowserAccessibilityTree } from './accessibility-tree.js'
+export type { AccessibilityNode } from './accessibility-tree.js'
+
+export { getMobileVisibleElements } from './mobile-elements.js'
+export type {
+  MobileElementInfo,
+  GetMobileElementsOptions
+} from './mobile-elements.js'
+
+export { getElements } from './get-elements.js'
+export type { VisibleElementsResult } from './get-elements.js'
